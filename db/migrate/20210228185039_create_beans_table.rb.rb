f60640caac8f8b5eefb7473c14df95e9c35c90ec
Lesly <leslyj2@gmail.com>
2021-02-28 14:24:04 -0500

    Created beans table

---
+++ db/migrate/20210228185039_create_beans_table.rb.rb
@@ -0,0 +1,11 @@
+class CreateBeansTable.rb < ActiveRecord::Migration[5.2]
+  def change
+    create_table :beans do |t|
+      t.string :name
+      t.string :brand
+      t.integer :grind_size
+      t.decimal :dose
+      t.decimal :extraction_volume
+      t.decimal :extraction_time
+  end
+end
