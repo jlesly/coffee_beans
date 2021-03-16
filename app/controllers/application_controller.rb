7d40d9e3671481cb4e30ca3cd82ae094058b4732
Lesly <leslyj2@gmail.com>
2021-03-16 13:16:26 -0400

    minor updates

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,4 @@
 require './config/environment'
-require 'rack-flash'
 
 class ApplicationController < Sinatra::Base
 
@@ -8,7 +7,6 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
     enable :sessions
     set :session_secret, "application_secret"
-    use Rack::Flash
   end
 
   get "/" do
@@ -23,6 +21,10 @@ class ApplicationController < Sinatra::Base
 
     def current_user
       @current_user ||= User.find_by(:id => session[:user_id]) if session[:user_id]
+    end
+
+    def flash_types
+      [:success, :notice, :warning, :error]
     end
   end
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ post '/signup' do
     @user = User.new(params)
     if @user.save
         session[:user_id]=@user.id
-        redirect "/epressos"
+        redirect "/espressos"
     else
         erb :'users/signup'
     end
@@ -36,7 +36,7 @@ end
 get '/logout' do
     if logged_in?
       session.clear
-      redirect "/login"
+      redirect "/"
     else
       redirect "/"
     end
